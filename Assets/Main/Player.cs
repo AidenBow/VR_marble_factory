a69b639d4335956ac4d3d68ecf565dbfa97ba158
AidenBow <AidenBow@users.noreply.github.com>
2020-02-27 21:42:45 -0800

    added trigger actions and interactable detection

--- Assets/Main/Player.cs
+++ Assets/Main/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class Player : MonoBehaviour
 {
     // Start is called before the first frame update
+    public SteamVR_Input_Sources handType;
     void Start()
     {
         
@@ -13,6 +15,11 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool triggerActive = SteamVR_Actions.default_GrabGrip.GetState(handType);
+
+        if (triggerActive)
+        {
+            Debug.Log("grip");
+        }
     }
 }
