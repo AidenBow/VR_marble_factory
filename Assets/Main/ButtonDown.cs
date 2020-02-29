86022893b0b316c75a4c321ca5930e43ff227313
AidenBow <AidenBow@users.noreply.github.com>
2020-02-28 18:52:51 -0800

    button steamvr script

---
+++ Assets/Main/ButtonDown.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonDown : MonoBehaviour
+{
+    public void ButtonPress()
+    {
+        Debug.Log("button down again");
+    }
+}
