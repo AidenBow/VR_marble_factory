294e5bef6b056f528ff56661a79b067438524287
AidenBow <AidenBow@users.noreply.github.com>
2020-02-28 22:47:50 -0800

    conveyor belt

---
+++ Assets/Main/Conveyor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Conveyor : MonoBehaviour
+{
+    public GameObject endPoint;
+    public float speed = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endPoint.transform.position, speed * Time.deltaTime);
+    }
+}
