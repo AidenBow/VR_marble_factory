8166a240ec910e90c1e9e6cf7cf59c1c395472c7
AidenBow <AidenBow@users.noreply.github.com>
2020-02-28 18:05:32 -0800

    cutom and steam vr throwable scripts(steamvr currently being used)

--- Assets/Main/Hand.cs
+++ Assets/Main/Hand.cs
@@ -26,13 +26,11 @@ public class Hand : MonoBehaviour
         //down
         if (m_GrabAction.GetStateDown(m_Pose.inputSource))
         {
-            print(m_Pose.inputSource);
             PickUp();
         }
         //up
         if (m_GrabAction.GetStateUp(m_Pose.inputSource))
         {
-            print(m_Pose.inputSource);
             Drop();
         }
     }
@@ -55,16 +53,64 @@ public class Hand : MonoBehaviour
 
     public void PickUp()
     {
+        //get nearest interactable
+        m_CurrentInteractable = GetNearestInteractable();
 
+        //null check
+        if (!m_CurrentInteractable)
+            return;
+
+        //already held check
+        if (m_CurrentInteractable.m_ActiveHand)
+            m_CurrentInteractable.m_ActiveHand.Drop();
+
+        //position to controller
+        m_CurrentInteractable.transform.position = transform.position;
+
+        //attach to fixedjoint
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        m_Joint.connectedBody = targetBody;
+
+        //set active hand
+        m_CurrentInteractable.m_ActiveHand = this;
     }
 
     public void Drop()
     {
+        //null check
+        if (!m_CurrentInteractable)
+            return;
+        //apply velocity
+        Rigidbody targetBody = m_CurrentInteractable.GetComponent<Rigidbody>();
+        targetBody.velocity = m_Pose.GetVelocity();
+        targetBody.angularVelocity = m_Pose.GetAngularVelocity();
+
+        //detach
+        m_Joint.connectedBody = null;
+
+        //clear vars
+        m_CurrentInteractable.m_ActiveHand = null;
+        m_CurrentInteractable = null;
 
     }
 
     private Interactable GetNearestInteractable()
     {
-        return null;
+        Interactable nearest = null;
+        float minDistance = float.MaxValue;
+        float distance = 0f;
+
+        foreach(Interactable interactable in m_ContactInteractables)
+        {
+            distance = (interactable.transform.position - transform.position).sqrMagnitude;
+
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                nearest = interactable;
+            }
+        }
+
+        return nearest;
     }
 }
