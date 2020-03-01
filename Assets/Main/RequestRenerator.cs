fd38d72a30058ed95315a9b215b92e4f2b6cc10b
AidenBow <AidenBow@users.noreply.github.com>
2020-03-01 15:31:19 -0800

    text and random request gen

---
+++ Assets/Main/RequestRenerator.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class RequestRenerator : MonoBehaviour
+{
+    public string request;
+    public TMP_Text text;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void GenNewShape()
+    {
+        float randomNum = Random.Range(0f, 9f);
+
+        if (randomNum < 4)
+        {
+            request = "rectangle";
+        } else if (randomNum < 7)
+        {
+            request = "sphere";
+        } else
+        {
+            request = "cylinder";
+        }
+
+        text.text = request;
+    }
+
+}
--- Assets/Main/TransverterCheck.cs
+++ Assets/Main/TransverterCheck.cs
@@ -31,9 +31,16 @@ public class TransverterCheck : MonoBehaviour
                     break;
                 case "rectangle":
                     GetComponent<MeshFilter>().mesh = cube;
+                    gameObject.transform.localScale = new Vector3(0.05f, 0.02f, 0.1f);
                     break;
                 case "cylinder":
                     GetComponent<MeshFilter>().mesh = cylinder;
+                    Destroy(GetComponent<BoxCollider>());
+                    Destroy(GetComponent<SphereCollider>());
+                    if (GetComponent<CapsuleCollider>() == null)
+                    {
+                        gameObject.AddComponent<CapsuleCollider>();
+                    }
                     break;
                 default:
                     Debug.Log("error: default case activated");
