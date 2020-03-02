c36f888d5a512d10721da232bbee358fae5d4e7d
AidenBow <AidenBow@users.noreply.github.com>
2020-03-01 18:13:06 -0800

    added more messages on fail and win

--- Assets/Main/BlockDetector.cs
+++ Assets/Main/BlockDetector.cs
@@ -22,14 +22,45 @@ public class BlockDetector : MonoBehaviour
     {
         if (other.transform.name == "placemat")
         {
+            float randomNum = Random.Range(0, 3);
             if (gameObject.GetComponent<CustomTags>().tags.Contains(requestObj.GetComponent<RequestGenerator>().request))
             {
-                requestText.GetComponent<TextMeshPro>().text = "Nice!";
+                switch(randomNum)
+                {
+                    case 1:
+                        requestText.GetComponent<TextMeshPro>().text = "Nice!";
+                        break;
+                    case 2:
+                        requestText.GetComponent<TextMeshPro>().text = "Awesome B)";
+                        break;
+                    case 0:
+                        requestText.GetComponent<TextMeshPro>().text = "GOOD WORK";
+                        break;
+                    default:
+                        requestText.GetComponent<TextMeshPro>().text = "okay";
+                        break;
+                }
+                
+                
                 StartCoroutine(Correct());
                 
             } else
             {
-                requestText.GetComponent<TextMeshPro>().text = "are you stupid or something???";
+                switch (randomNum)
+                {
+                    case 1:
+                        requestText.GetComponent<TextMeshPro>().text = "are you stupid or something???";
+                        break;
+                    case 2:
+                        requestText.GetComponent<TextMeshPro>().text = "uh oh idiot alert";
+                        break;
+                    case 0:
+                        requestText.GetComponent<TextMeshPro>().text = "no!";
+                        break;
+                    default:
+                        requestText.GetComponent<TextMeshPro>().text = "no";
+                        break;
+                }
                 StartCoroutine(Wrong());
             }
 
